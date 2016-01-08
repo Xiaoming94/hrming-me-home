83d39d6df409b7960d040a6427643b4091ebb5a5
Henry Yang <henyan94@gmail.com>
2016-01-08 17:47:46 +0000

    Initial Commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+   "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html>
+<head>
+    <link href="styles.css" rel="stylesheet" />
+    <title><?php echo "XiaoMing Home"?></title>
+</head>
+<body>
+    <div class="container">
+        <div class="header-container">
+            <header>
+                <h1><?php echo "Välkomna"?></h1>
+            </header>
+        </div>
+        <div class="presentation-text">
+        <?php echo "Det här är en sida av Henry Yang som är fortfarande under utväckling.<br />"?>
+        <?php echo "Men du kan titta in länkarna nedan :) "?>
+        </div>
+        <ul>
+            <li><a href="http://blog.hrming.me"><?php echo "Min Blogg"?></a></li>
+        </ul>
+    </div>
+<body>
+</html>
