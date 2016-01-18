9705d80a4eb18b444c2eec20ab033b583c2d1c33
Henry <henyan94@gmail.com>
2016-01-18 11:39:59 +0100

    Changed some fonts

--- index.php
+++ index.php
@@ -3,9 +3,9 @@
 <html>
 <head>
     <link href="styles.css" rel="stylesheet" />
-    <link rel="stylesheet" type="text/css"
-    href="//fonts.googleapis.com/css?family=Josefin+Slab" />
-    <title><?php echo "XiaoMing Home"?></title>
+    <link href='https://fonts.googleapis.com/css?family=Slabo+27px|Raleway:400,700,500|Droid+Serif:400,400italic,700,700italic'
+rel='stylesheet' type='text/css' />
+<title><?php echo "XiaoMing Home"?></title>
 </head>
 <body>
     <div class="container">
