28cedb2147859833d9a1f313d4e94ed5765e367a
Henry <henyan94@gmail.com>
2016-02-07 17:13:58 +0100

    Changed block table description

--- db/migrate/20160207152921_create_blocks.rb
+++ db/migrate/20160207152921_create_blocks.rb
@@ -1,10 +1,13 @@
 class CreateBlocks < ActiveRecord::Migration
-  def change
+  def up
     create_table :blocks do |t|
       t.string "name", :null => false
-      t.column "content", :string, :null => false
+      t.column "content", :longtext
       t.column "usage", :string
       t.timestamps null: false
     end
+  end
+  def down
+    drop_table :blocks
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -14,11 +14,11 @@
 ActiveRecord::Schema.define(version: 20160207152921) do
 
   create_table "blocks", force: :cascade do |t|
-    t.string   "name",       limit: 255, null: false
-    t.string   "content",    limit: 255, null: false
+    t.string   "name",       limit: 255,        null: false
+    t.text     "content",    limit: 4294967295
     t.string   "usage",      limit: 255
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
+    t.datetime "created_at",                    null: false
+    t.datetime "updated_at",                    null: false
   end
 
 end
